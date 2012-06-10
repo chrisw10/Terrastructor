93a0acb47877bc416ecbc9f7f462275574ded8f6
Joshua Rodgers <bytemr@gmail.com>
2012-06-09 21:15:17 -0600

    Created initial Heightmap class and tests.

--- src/Byte.Terrastructor.Heightmap.Tests/HeightmapTest.cs
+++ src/Byte.Terrastructor.Heightmap.Tests/HeightmapTest.cs
@@ -9,5 +9,41 @@ namespace Byte.Terrastructor.Heightmap.Tests
     [TestFixture]
     public class HeightmapTest
     {
+        [Test]
+        public void TestCreateHeightmap()
+        {
+            var heightmap = new Heightmap("test_create_heightmap.bmp");
+
+            const int expectedWidth = 32;
+            const int expectedHeight = 32;
+
+            Assert.AreEqual(expectedWidth, heightmap.Width);
+            Assert.AreEqual(expectedHeight, heightmap.Height);
+        }
+
+        [Test]
+        public void TestHeightmapData()
+        {
+            var heightmap = new Heightmap("test_heightmap_data.bmp");
+
+            const byte expectedHighByte = 255;
+            const byte expectedLowByte = 0;
+
+            Assert.AreEqual(expectedHighByte, heightmap[0, 0]);
+            Assert.AreEqual(expectedHighByte, heightmap[0, 1]);
+            Assert.AreEqual(expectedHighByte, heightmap[1, 0]);
+            Assert.AreEqual(expectedLowByte, heightmap[1, 1]);
+        }
+
+        [Test]
+        public void TestOutOfBoundsData()
+        {
+            var heightmap = new Heightmap("test_heightmap_data.bmp");
+
+            const byte expectedLowByte = 0;
+
+            Assert.AreEqual(expectedLowByte, heightmap[-1, -1]);
+            Assert.AreEqual(expectedLowByte, heightmap[2, 2]);    
+        }
     }
 }
---
+++ src/Byte.Terrastructor.Heightmap/Heightmap.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace Byte.Terrastructor.Heightmap
+{
+    public class Heightmap
+    {
+        private const int BytesPerPixel = 3;
+
+        private readonly Bitmap _heightmapBitmap;
+        private byte[,] _heightPoints;
+
+        public int Width
+        {
+            get
+            {
+                return _heightPoints.GetLength(0);
+            }
+        }
+
+        public int Height
+        {
+            get
+            {
+                return _heightPoints.GetLength(1);
+            }
+        }
+
+        public Heightmap(string filename)
+        {
+            //NOTE: The second parameter is false in order to turn off "color correction" which is used by default.
+            _heightmapBitmap = new Bitmap(filename, false);
+
+            ProcessHeightmap();
+        }
+
+        private void ProcessHeightmap()
+        {
+            _heightPoints = new byte[_heightmapBitmap.Width, _heightmapBitmap.Height];
+
+            BitmapData data = _heightmapBitmap.LockBits(new Rectangle(0,
+                                                                      0,
+                                                                      _heightmapBitmap.Width,
+                                                                      _heightmapBitmap.Height),
+                                                        ImageLockMode.ReadOnly,
+                                                        PixelFormat.Format24bppRgb);
+
+            IntPtr startPointer = data.Scan0;
+
+            var width = data.Width*BytesPerPixel;
+
+            for(int yPosition = 0; yPosition < _heightmapBitmap.Height; ++yPosition)
+            {
+                var linePointer = startPointer + (yPosition*data.Stride);
+                var rgbData = new byte[width];
+                Marshal.Copy(linePointer, rgbData, 0, rgbData.Length);
+
+                for(int index = 0; index < rgbData.Length; index += BytesPerPixel)
+                {
+                    var xPosition = index/BytesPerPixel;
+
+                    _heightPoints[xPosition, yPosition] = rgbData[index];
+                }
+            }
+        }
+
+        public byte this[int x, int y]
+        {
+            get
+            {
+                if(x < 0 || y < 0 || x >= Width || y >= Height)
+                {
+                    return 0;
+                }
+
+                return _heightPoints[x, y];
+            }
+        }
+    }
+}
