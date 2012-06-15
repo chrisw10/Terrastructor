2f403a3192209749c0c41de8cfce3f85a7afb954
Joshua Rodgers <bytemr@gmail.com>
2012-06-14 20:04:11 -0600

    Added the foundations for transitions between texture groups and processing heightmaps and texture groups into actual terrain and corresponding textures

---
+++ src/Byte.Terrastructor.Terrain.Model.Tests/TextureGroupTransitionTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Byte.Terrastructor.Terrain.Model.Tests
+{
+    [TestFixture]
+    public class TextureGroupTransitionTest
+    {
+    }
+}
---
+++ src/Byte.Terrastructor.Terrain.Model/HeightmapTextureGroupProcessor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Byte.Terrastructor.Heightmap;
+
+namespace Byte.Terrastructor.Terrain.Model
+{
+    public class HeightmapTextureGroupProcessor
+    {
+        private readonly IHeightmap _heightmap;
+        private readonly ITextureGroupResolver _textureGroupResolver;
+
+        public HeightmapTextureGroupProcessor(IHeightmap heightmap, ITextureGroupResolver textureGroupResolver)
+        {
+            _heightmap = heightmap;
+            _textureGroupResolver = textureGroupResolver;
+        }
+    }
+}
